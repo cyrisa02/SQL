4d36c7f9a9edfe6afa05d114b3d1091b475b05e0
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-01-31 18:12:54 +0100

    premiers pas en sql

---
+++ recette.sql
@@ -0,0 +1,19 @@
+-- SQLite
+CREATE TABLE recipes (
+title VARCHAR(150),
+slug VARCHAR(50),
+content TEXT,
+duration SMALLINT,
+online BOOLEAN,
+created_at DATETIME
+);
+
+-- recipes 
+/* -title
+-content
+-slug
+-duration (in min)
+- online
+- created_at
+*/
+
---
+++ titre_tableau.sql
@@ -0,0 +1,7 @@
+-- SQLite
+CREATE TABLE posts (
+title VARCHAR(150),
+ content TEXT,
+ category VARCHAR(50),
+ created_at DATETIME
+ );
